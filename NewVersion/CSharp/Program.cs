f3d3a924a0926e2180539b7f0cf4976ec067afa7
yoshi <44921082+yoshi-782@users.noreply.github.com>
2020-12-14 08:18:15 +0900

    Update Program.cs

--- NewVersion/CSharp/Program.cs
+++ NewVersion/CSharp/Program.cs
@@ -3,11 +3,11 @@ using System.Linq;
 using System.Text;
 
 string DakutenConv(string arg){
-            return string.Join("", Enumerable.ToList<char>(arg).Select((c) =>{
-                var normalize = c.ToString().Normalize(NormalizationForm.FormD);
-                return normalize.Length > 1 ? normalize : $"{normalize}゛";
-            }).ToArray());
-        }
+    return string.Join("", Enumerable.ToList<char>(arg).Select((c) =>{
+        var normalize = c.ToString().Normalize(NormalizationForm.FormD);
+        return normalize.Length > 1 ? normalize : $"{normalize}゛";
+    }).ToArray());
+}
 
 string EasyDakutenConv(string arg){
     var result = "";
