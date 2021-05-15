b1da41972b65aabf35cfd86faf8ec3d0ee921d7d
yoshi <44921082+yoshi-782@users.noreply.github.com>
2021-05-15 21:59:00 +0900

    substringで1文字にするよう修正

--- NewVersion/JavaScript/dakutenConv.js
+++ NewVersion/JavaScript/dakutenConv.js
@@ -1,7 +1,7 @@
 function dakuten(arg){
     return [...arg].map((a) => {
         const normalize = a.normalize("NFD")
-        return normalize.length > 1 ? normalize : `${normalize}゛`;
+        return normalize.length > 1 ? `${normalize.substring(0, 1)}゛` : `${normalize}゛`;
     }).join('')
 }
 
