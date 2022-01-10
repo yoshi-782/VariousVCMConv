046da8cac7e85578266a24eaa70155ab950e1608
yoshi <yoshiki.0828.qqq@gmail.com>
2022-01-10 23:52:33 +0900

    conv関数を入れ子に修正

--- NewVersion/Python/dakutenConv.py
+++ NewVersion/Python/dakutenConv.py
@@ -1,11 +1,11 @@
 import unicodedata
 
-def dakuten(arg: str):
-    return "".join(map(lambda a: conv(a), list(arg)))
-
-def conv(a: str) -> str:
-    normalize = unicodedata.normalize("NFD", a)
-    return f"{normalize[:1]}゛" if len(normalize) > 1 else f"{normalize}゛"
+def dakuten(text: str) -> str:
+    def conv(a: str) -> str:
+        normalize = unicodedata.normalize("NFD", a)
+        return f"{normalize[:1]}゛" if len(normalize) > 1 else f"{normalize}゛"
+    
+    return "".join(map(lambda a: conv(a), list(text)))
 
 if __name__ == '__main__':
     print(dakuten("だくてんテスト"))
