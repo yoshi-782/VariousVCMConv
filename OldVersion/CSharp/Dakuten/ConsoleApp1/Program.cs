e5102b02bab1c92fb32f7356d2bbf8045f3e9d98
yoshi <yoshiki.0828.qqq@gmail.com>
2022-05-26 01:13:49 +0900

    処理を効率的に修正

---
+++ OldVersion/CSharp/Dakuten/ConsoleApp1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(new Dakuten().Conv("だくてんへんかん"));
+        }
+    }
+
+    /// <summary>
+    /// 濁点変換
+    /// </summary>
+    public class Dakuten
+    {
+        /// <summary>
+        /// 濁点変換用データ
+        /// </summary>
+        private readonly Mark marks = new();
+
+        /// <summary>
+        /// Json用構造体
+        /// </summary>
+        public class Mark
+        {
+            [JsonPropertyName("dakuten")]
+            public string[] Dakuten { get; set; }
+            [JsonPropertyName("nomal")]
+            public string[] Nomal { get; set; }
+            [JsonPropertyName("skip")]
+            public string[] Skip { get; set; }
+
+            public Mark()
+            {
+                // 初期化
+                this.Dakuten = Array.Empty<string>();
+                this.Nomal = Array.Empty<string>();
+                this.Skip = Array.Empty<string>();
+            }
+        }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="jsonFile">Jsonファイル名</param>
+        public Dakuten(string jsonFile = "mark.json")
+        {
+            // jsonファイル読み込み
+            var json = File.ReadAllText(jsonFile);
+            marks = JsonSerializer.Deserialize<Mark>(json)!;
+        }
+
+        /// <summary>
+        /// 濁点変換
+        /// </summary>
+        /// <param name="value">変換文字</param>
+        /// <returns>変換された文字</returns>
+        public string Conv(string value)
+        {
+            return string.Join("", value.ToCharArray()
+            .Select(c => c.ToString())
+            .Select(c =>
+            {
+                if (Array.IndexOf(marks.Skip, c) > -1)
+                {
+                    return $"{c}";
+                }
+                else if (Array.IndexOf(marks.Dakuten, c) > -1)
+                {
+                    return $"{marks.Nomal[Array.IndexOf(marks.Dakuten, c)]}゛";
+                }
+                else
+                {
+                    return $"{c}゛";
+                }
+            }));
+        }
+    }
+}
